f43b7aa26612851ee5e6629f0efbbcf373e5fa90
yeangyeang <1819196680@qq.com>
2018-06-09 13:28:53 +0800

    6/9

--- src/com/sxt/renthouse/dao/HouseCollectionDao.java
+++ src/com/sxt/renthouse/dao/HouseCollectionDao.java
@@ -2,7 +2,7 @@ package com.sxt.renthouse.dao;
 
 import java.util.List;
 
-import com.sxt.renthouse.entity.HouseCollection;
+import com.sxt.renthouse.entity.Housecollection;
 import com.sxt.renthouse.entity.User;
 
 public interface HouseCollectionDao {
@@ -11,21 +11,21 @@ public interface HouseCollectionDao {
 	 * @param HouseCollection 
 	 * @return 返回受影响的行
 	 */
-	public int addHouseCollection(HouseCollection hc);
+	public int addHouseCollection(Housecollection hc);
 	
 	/**
 	 * 根据条件查询所有的餐桌
 	 * @param HouseCollection  条件
 	 * @return
 	 */
-	public List<HouseCollection> getAllHouseCollection(HouseCollection hc);
+	public List<Housecollection> getAllHouseCollection(Housecollection hc);
 	
 	/**
 	 * 根据id 跟新一条记录
 	 * @param HouseCollection
 	 * @return
 	*/
-	public int updateHouseCollection(HouseCollection hc);
+	public int updateHouseCollection(Housecollection hc);
 	
 	/**
 	 * 根据ID删除一条记录
