ca215ad65b08a633af28af2ef4710373727aac90
cg980925441 <980925441@qq.com>
2018-06-09 14:50:56 +0800

    s

--- src/com/sxt/renthouse/entity/User.java
+++ src/com/sxt/renthouse/entity/User.java
@@ -2,93 +2,77 @@ package com.sxt.renthouse.entity;
 
 public class User {
 	//用户编号 主键
-    private String uId;
+    private String u_Id;
     //密码
-    private String uPwd;
+    private String u_Pwd;
     //用户昵称
-    private String uName;
+    private String u_Name;
     //性别
-    private String uSex;
+    private String u_Sex;
     //图片
-    private String uPhoto;
+    private String u_Photo;
     //等级
-    private String uGrade;
+    private String u_Grade;
     //手机号码
-    private String uPhone;
+    private String u_Phone;
     //用户种类
-    private String uType;
+    private String u_Type;
     //状态
-    private Integer uStatus;
+    private Integer u_Status;
+	public String getU_Id() {
+		return u_Id;
+	}
+	public void setU_Id(String u_Id) {
+		this.u_Id = u_Id;
+	}
+	public String getU_Pwd() {
+		return u_Pwd;
+	}
+	public void setU_Pwd(String u_Pwd) {
+		this.u_Pwd = u_Pwd;
+	}
+	public String getU_Name() {
+		return u_Name;
+	}
+	public void setU_Name(String u_Name) {
+		this.u_Name = u_Name;
+	}
+	public String getU_Sex() {
+		return u_Sex;
+	}
+	public void setU_Sex(String u_Sex) {
+		this.u_Sex = u_Sex;
+	}
+	public String getU_Photo() {
+		return u_Photo;
+	}
+	public void setU_Photo(String u_Photo) {
+		this.u_Photo = u_Photo;
+	}
+	public String getU_Grade() {
+		return u_Grade;
+	}
+	public void setU_Grade(String u_Grade) {
+		this.u_Grade = u_Grade;
+	}
+	public String getU_Phone() {
+		return u_Phone;
+	}
+	public void setU_Phone(String u_Phone) {
+		this.u_Phone = u_Phone;
+	}
+	public String getU_Type() {
+		return u_Type;
+	}
+	public void setU_Type(String u_Type) {
+		this.u_Type = u_Type;
+	}
+	public Integer getU_Status() {
+		return u_Status;
+	}
+	public void setU_Status(Integer u_Status) {
+		this.u_Status = u_Status;
+	}
 
-    public String getuId() {
-        return uId;
-    }
-
-    public void setuId(String uId) {
-        this.uId = uId == null ? null : uId.trim();
-    }
-
-    public String getuPwd() {
-        return uPwd;
-    }
-
-    public void setuPwd(String uPwd) {
-        this.uPwd = uPwd == null ? null : uPwd.trim();
-    }
-
-    public String getuName() {
-        return uName;
-    }
-
-    public void setuName(String uName) {
-        this.uName = uName == null ? null : uName.trim();
-    }
-
-    public String getuSex() {
-        return uSex;
-    }
-
-    public void setuSex(String uSex) {
-        this.uSex = uSex == null ? null : uSex.trim();
-    }
-
-    public String getuPhoto() {
-        return uPhoto;
-    }
-
-    public void setuPhoto(String uPhoto) {
-        this.uPhoto = uPhoto == null ? null : uPhoto.trim();
-    }
-
-    public String getuGrade() {
-        return uGrade;
-    }
-
-    public void setuGrade(String uGrade) {
-        this.uGrade = uGrade == null ? null : uGrade.trim();
-    }
-
-    public String getuPhone() {
-        return uPhone;
-    }
-
-    public void setuPhone(String uPhone) {
-        this.uPhone = uPhone == null ? null : uPhone.trim();
-    }
-
-    public String getuType() {
-        return uType;
-    }
-
-    public void setuType(String uType) {
-        this.uType = uType == null ? null : uType.trim();
-    }
-
-    public Integer getuStatus() {
-        return uStatus;
-    }
-
-    public void setuStatus(Integer uStatus) {
-        this.uStatus = uStatus;
-    }
+    
 }
