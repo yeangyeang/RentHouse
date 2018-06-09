83a0291294a01a4feb6aa3e147358609423d8cec
cg980925441 <980925441@qq.com>
2018-06-09 17:08:53 +0800

    6

--- src/com/sxt/renthouse/dao/impl/TestUserDao.java
+++ src/com/sxt/renthouse/dao/impl/TestUserDao.java
@@ -54,8 +54,69 @@ public class TestUserDao {
 			userList = qr.query(DBCommon.getConn(), sql, new BeanListHandler<User>(User.class),pageUtils.getStartRow(),pageUtils.getPageSize());
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			
+		}
+		return userList;
+	}
+	
+	/**
+	 * 根据用户ID查询用户数据
+	 * @param pageUtils	分页工具类
+	 * @param id		用户id
+	 * @return			查询到的用户数据集合
+	 */
+	public static List<User> findUserById(PageUtils pageUtils,String id){
+		//创建查询器赋给数据库
+		QueryRunner qr= null;
+		String sql = "select * from user where u_id = ?";
+		
+		qr = ComPoolUtil.getQueryRunner();
+		List<User> userList=null;
+		try {
+			userList = qr.query(DBCommon.getConn(), sql, new BeanListHandler<User>(User.class),id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userList;
+	}
+	
+	/**
+	 * 根据用户昵称查询用户数据
+	 * @param pageUtils	分页工具类
+	 * @param id		用户昵称
+	 * @return			查询到的用户数据集合
+	 */
+	public static List<User> findUserByName(PageUtils pageUtils,String name){
+		//创建查询器赋给数据库
+		QueryRunner qr= null;
+		String sql = "select * from user where u_name = ?";
+		
+		qr = ComPoolUtil.getQueryRunner();
+		List<User> userList=null;
+		try {
+			userList = qr.query(DBCommon.getConn(), sql, new BeanListHandler<User>(User.class),name);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userList;
+	}
+	
+	/**
+	 * 根据用户电话查询用户数据
+	 * @param pageUtils	分页工具类
+	 * @param id		用户电话
+	 * @return			查询到的用户数据集合
+	 */
+	public static List<User> findUserByPhone(PageUtils pageUtils,String phone){
+		//创建查询器赋给数据库
+		QueryRunner qr= null;
+		String sql = "select * from user where u_phone = ?";
+		
+		qr = ComPoolUtil.getQueryRunner();
+		List<User> userList=null;
+		try {
+			userList = qr.query(DBCommon.getConn(), sql, new BeanListHandler<User>(User.class),phone);
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		return userList;
 	}
